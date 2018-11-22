3e722c2bd87198845b89d6d4ef032e953957d7c3
quintenbecker <quintenworks@gmail.com>
2018-11-22 16:48:45 +0100

    first version hillclimber

--- classes/hill_climber.py
+++ classes/hill_climber.py
@@ -1,23 +1,71 @@
+import random
+import pprint
+
 class hill_climber:
 
     """
     Uses an already made graph, and optimalises it by changing
     the color of the neighbours and checking the result
     """
-    possible_colors = [1, 2, 3, 4, 5, 6, 7]
 
     def __init__(self, input):
         self.input = input
+        self.all_colors = [1, 2, 3, 4, 5, 6, 7]
 
+        # print(self.input.nodes)
+
+    def diff(self, first, second):
+        second = set(second)
+        return [item for item in first if item not in second]
 
     def hillclimber (self):
         gc = self.input.check_graph()
-        print(gc[1])
-        neighbour_colors = []
-        # while not self.input.found() == True:
-        for node in self.input.wrong_nodes:
-            pass
+        # print(f"first gc: {gc}")
+        while not self.input.found():
+            gc = self.input.check_graph()
+            # print(f"loop gc: {gc}")
+            i = 0
+            for node in self.input.wrong_nodes:
+                # print(node.name)
+                neighbour_colors = []
+                for neighbour in node.neighbours:
+                    neighbour_colors.append(neighbour.color)
+                # print(neighbour_colors)
 
+                leftover_colors = (self.diff(self.all_colors, neighbour_colors))
+                # print(leftover_colors)
+                new_color = self.apply_color(leftover_colors)
+
+
+                # print("----------")
+                # print(f" old: {gc[1][0][i].color}")
+                gc[1][0][0].color = new_color
+                # print(gc[1][0][i].color)
+                i += 1
+                self.input.check_graph()
+
+
+        pp = pprint.PrettyPrinter()
+        pp.pprint(self.input.nodes)
+
+
+    def apply_color (self, leftover_colors):
+        color = random.choice(leftover_colors)
+        # print(f"new color {color}")
+        return color
+
+
+
+
+
+
+
+            # find wrong node
+            # find color of neighborig nodes
+            # remove these colours from possible_colors(list)
+            # apply left over color to nodes
+
+            #check again
 
 
 
