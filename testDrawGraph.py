75326985aa93e32d10cc02bb2ed8ab5cfc892ba5
danielBusinessAnalytics <44001174+danielBusinessAnalytics@users.noreply.github.com>
2018-12-01 14:17:38 +0100

    update

--- testDrawGraph.py
+++ testDrawGraph.py
@@ -110,9 +110,9 @@ def draw_colored_graph(graph):
 
 
 if __name__ == "__main__":
-    graphUkr = Graph(input)
+    graphUkr = Graph(input2)
     graphUkr.create_graph()
-    kempe = Kempe(6)
+    kempe = Kempe(4)
     graphUkr = kempe.execute_kempe(graphUkr)
     print(graphUkr.nodes)
     draw_colored_graph(graphUkr)
